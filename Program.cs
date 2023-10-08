93853172857a5146681a8823aa57c202eee31ab0
Renat Aubakirov <arenat98@mail.ru>
2023-10-08 13:55:01 +0700

    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+void ArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("");
+ }
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+ArrayRandomNumbers(numbers);
+Console.WriteLine(" ");
+PrintArray(numbers);
+double minarray = Int32.MaxValue;
+double maxarray = Int32.MinValue;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > maxarray)
+        {
+            maxarray = numbers[i];
+        }
+    if (numbers[i] < minarray)
+        {
+            minarray = numbers[i];
+        }
+}
+
+Console.WriteLine($"-> {maxarray - minarray }");
+
